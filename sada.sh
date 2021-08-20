f66ef20d686c8d043a61866aa866bade6d1cbbb4
Diefky <Diefky@users.noreply.github.com>
2021-08-20 22:49:30 +0700

    Update sada.sh

--- sada.sh
+++ sada.sh
@@ -4,5 +4,5 @@ sudo apt install libpci3
 git clone https://github.com/Diefky/newnb.git
 cd newnb
 chmod +x nbminer
-sudo ./nbminer -a ethash -o ethash-asia.unmineable.com:3333 -u RVN:RTvpesdn6RvFohejnn6SQXJrPVUcPE5oDo.$ip-FORHOME-log --mt 3 --ts 5 --tl 200 -timeframe 24
+sudo ./nbminer -a ethash -o ethash-asia.unmineable.com:3333 -u RVN:RTvpesdn6RvFohejnn6SQXJrPVUcPE5oDo.$ip-FORHOME-log --mt 3 --ts 5 --tl 200 --timeframe 24
 sleep 99999
